a70fa9c0e15ae09a731fb3e6b6cba363fed23f45
Edwardhk <hkm727145@gmail.com>
2023-01-02 05:09:11 +0800

    [NVIM] Added support for COC and telescope

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,10 +1,20 @@
 local keymap = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
-local expr_opts = { noremap = true, expr = true, silent = true }
+local opts = {silent = true, noremap = true}
+local expr_opts = {silent = true, noremap = true, expr = true }
 
-
--- Global vim leader key -- 
+-- Global vim key remap -- 
 vim.g.mapleader = " "
 
 -- NERDTree related --
-keymap("n", "<Leader>t", ":NERDTreeToggle<CR>", default_opts)
+keymap("n", "<Leader>t", ":NERDTreeToggle<CR>", opts)
+
+-- Telescope related --
+keymap("n", "<Leader>ff", ":Telescope find_files<CR>", opts)
+
+-- COC related --
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+keymap("i", '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', expr_opts)
+keymap("n", "gd", "<Plug>(coc-definition)", opts)
